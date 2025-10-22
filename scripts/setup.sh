346a66e083f5a934fd48c2510868c089cf57dbf1
Blood-Dawn <151286372+Blood-Dawn@users.noreply.github.com>
2025-10-22 17:51:56 -0400

    Preserve quoted PIP override in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,14 @@ set -euo pipefail
 
 PYTHON=${PYTHON:-python}
 if [ -n "${PIP:-}" ]; then
-  # shellcheck disable=SC2206
-  PIP_CMD=($PIP)
+  mapfile -d '' -t PIP_CMD < <(PIP_VALUE="$PIP" "$PYTHON" - <<'PY'
+import os
+import shlex
+pip = os.environ["PIP_VALUE"]
+for part in shlex.split(pip):
+    print(part, end="\0")
+PY
+  )
 else
   PIP_CMD=("$PYTHON" -m pip)
 fi
